18bf6552a1b796e8e2bafee02e3b32cf1aaa14f3
matosauer <livepictures@gmail.com>
2025-02-19 13:03:54 +0200

    styling the navbar

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,5 +1,5 @@
 import { DarkMode, LightMode, ShoppingCart } from "@mui/icons-material";
-import { AppBar, Badge, IconButton, List, ListItem, Toolbar, Typography } from "@mui/material";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Toolbar, Typography } from "@mui/material";
 import { NavLink } from "react-router";
 
 const midLinks = [
@@ -13,6 +13,19 @@ const rightLinks = [
     { title: 'register', path: '/register' }
 ]
 
+
+const navStyles ={
+    color: 'inherit', 
+    typography: 'h6',
+    textDecoration: 'none',
+    '&:hover': {
+        color: 'grey.500'
+    },
+    '&.active': {
+        color: '#baecf9'
+    }
+}
+
 type Props = {
     darkMode: boolean
     switchMode: () => void
@@ -21,11 +34,13 @@ type Props = {
 export default function NavBar({darkMode, switchMode}: Props) {
     return (
         <AppBar position="fixed">
-            <Toolbar>
-                <Typography component={NavLink} to='/' variant="h6">RE-STORE</Typography>
-                <IconButton onClick={() => switchMode()}>
-                    {darkMode ? <DarkMode /> : <LightMode sx={{color: 'yellow'}} />}
-                </IconButton>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                <Box display='flex' alignItems='center'>
+                    <Typography component={NavLink} to='/' sx={navStyles} variant="h6">RE-STORE</Typography>
+                    <IconButton onClick={() => switchMode()}>
+                        {darkMode ? <DarkMode /> : <LightMode sx={{color: 'yellow'}} />}
+                    </IconButton>
+                </Box>
 
                 <List sx={{display: 'flex'}}>
                     {midLinks.map(({title, path}) => (
@@ -33,31 +48,33 @@ export default function NavBar({darkMode, switchMode}: Props) {
                             component={NavLink}
                             to={path}
                             key={path}
-                            sx={{color: 'inherit', typography: 'h6'}}
+                            sx={navStyles}
                         >
                             {title.toUpperCase()}
                         </ListItem>
                     ))}
                 </List>
 
-                <IconButton size="large" sx={{color: 'inherit'}}>
-                    <Badge badgeContent={4} color="secondary">
-                        <ShoppingCart color="action" />
-                    </Badge>
-                </IconButton>
-
-                <List sx={{display: 'flex'}}>
-                    {rightLinks.map(({title, path}) => (
-                        <ListItem
-                            component={NavLink}
-                            to={path}
-                            key={path}
-                            sx={{color: 'inherit', typography: 'h6'}}
-                        >
-                            {title.toUpperCase()}
-                        </ListItem>
-                    ))}
-                </List>
+                <Box display='flex' alignItems='center'>
+                    <IconButton size="large" sx={{color: 'inherit'}}>
+                        <Badge badgeContent={4} color="secondary">
+                            <ShoppingCart color="action" />
+                        </Badge>
+                    </IconButton>
+                    
+                    <List sx={{display: 'flex'}}>
+                        {rightLinks.map(({title, path}) => (
+                            <ListItem
+                                component={NavLink}
+                                to={path}
+                                key={path}
+                                sx={navStyles}
+                            >
+                                {title.toUpperCase()}
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
 
             </Toolbar>
         </AppBar>
