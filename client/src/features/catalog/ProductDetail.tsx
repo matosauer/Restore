895fef1aab2009bc1b74d675a1aac719dcbae945
matosauer <livepictures@gmail.com>
2025-02-19 14:10:52 +0200

    Set up product details page items

--- client/src/features/catalog/ProductDetail.tsx
+++ client/src/features/catalog/ProductDetail.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Product } from "../../app/models/product";
+import { Button, Divider, Grid2, Table, TableBody, TableContainer, TextField, Typography } from "@mui/material";
 
 export default function ProductDetail() {
 
@@ -14,8 +15,53 @@ export default function ProductDetail() {
           .catch(error => console.log(error))
   }, [id])
 
+  if(!product) return <div>Loading...</div>
 
   return (
-    <div>{product?.name}</div>
+    <Grid2 container spacing={6} maxWidth='lg' sx={{ mx: 'auto' }}>
+      <Grid2 size={6}>
+        <img src={product?.pictureUrl} alt={product.name} style={{ width: '100%' }} />
+      </Grid2>
+      <Grid2 size={6}>
+        <Typography variant="h3">{product.name}</Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Typography variant="h4" color='secondary'>${(product.price / 100).toFixed(2)}</Typography>
+        <TableContainer>
+          <Table sx={{
+            '& td': {fontSize: '1rem'}
+          }}>
+            <TableBody>
+              Table goes here
+
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        <Grid2 container spacing={2} marginTop={3}>
+          <Grid2 size={6}>
+            <TextField
+              variant="outlined"
+              type="number"
+              label='Quantity in basket'
+              fullWidth
+              defaultValue={1}
+            />
+          </Grid2>
+          <Grid2 size={6}>
+            <Button
+              color='primary'                            
+              size='large'
+              variant='contained'
+              fullWidth
+            >
+              Add to basket
+            </Button>
+          </Grid2>
+        </Grid2>
+
+
+
+      </Grid2>
+    </Grid2>
   )
 }
