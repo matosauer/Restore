7f9b89c1558dbc506f07ed1caefa43d8355949eb
matosauer <livepictures@gmail.com>
2025-07-15 10:46:30 +0300

    Add Basket and BasketItem entities with migration

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -8,6 +8,7 @@ namespace API.Data;
 public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(options)
 {
     public DbSet<Product> Products { get; set; }
+    public DbSet<Basket> Baskets { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -15,8 +16,8 @@ public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(op
 
         builder.Entity<IdentityRole>()
         .HasData(
-            new IdentityRole { Id = "08603f0a-eb32-4f0e-bb61-14571f9460c3", Name = "Admin", NormalizedName = "ADMIN" },            
-            new IdentityRole { Id = "365ee8d5-a914-4dd0-866f-5c6ef6a82df3", Name = "Member", NormalizedName = "MEMBER" } 
+            new IdentityRole { Id = "08603f0a-eb32-4f0e-bb61-14571f9460c3", Name = "Admin", NormalizedName = "ADMIN" },
+            new IdentityRole { Id = "365ee8d5-a914-4dd0-866f-5c6ef6a82df3", Name = "Member", NormalizedName = "MEMBER" }
         );
     }
 }
