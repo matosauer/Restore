43e9d9b99087884b3e0349880c7beff6967e2ba2
matosauer <livepictures@gmail.com>
2025-07-17 15:13:55 +0300

    Invalidate basket cache when adding an item

--- client/src/features/basket/basketApi.ts
+++ client/src/features/basket/basketApi.ts
@@ -8,13 +8,22 @@ export const basketApi = createApi({
     tagTypes: ['Basket'],
     endpoints: (builder) => ({
         fetchBasket: builder.query<Basket, void>({
-            query: () => 'basket'
+            query: () => 'basket',
+            providesTags: ['Basket']
         }),
         addBasketItem: builder.mutation<Basket, { productId: number, quantity: number }>({
             query: ({ productId, quantity }) => ({
                 url: `basket?productId=${productId}&quantity=${quantity}`,
                 method: 'POST'
-            })
+            }),
+            onQueryStarted: async (_, { dispatch, queryFulfilled }) => {
+                try {
+                    await queryFulfilled;
+                    dispatch(basketApi.util.invalidateTags(['Basket']))
+                } catch (error) {
+                    console.log(error);
+                }
+            }
         }),
         removeBasketItem: builder.mutation<Basket, { productId: number, quantity: number }>({
             query: ({ productId, quantity }) => ({
